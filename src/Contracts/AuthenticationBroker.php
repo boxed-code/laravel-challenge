4e03ae4b75dca215028425b1c5c9956b996aec2c
Oliver Green <green2go@gmail.com>
2019-07-23 09:44:38 +0100

    Added method signatures to 'AuthenticationBroker' contract.

--- src/Contracts/AuthenticationBroker.php
+++ src/Contracts/AuthenticationBroker.php
@@ -2,6 +2,9 @@
 
 namespace BoxedCode\Laravel\TwoFactor\Contracts;
 
+use BoxedCode\Laravel\TwoFactor\Contracts\Challengeable;
+use Illuminate\Contracts\Events\Dispatcher as EventDispatcher;
+
 interface AuthenticationBroker
 {
     const INVALID_METHOD = 'invalid_method',
@@ -14,6 +17,20 @@ interface AuthenticationBroker
           USER_CANNOT_ENROL = 'user_cannot_enrol',
           USER_CHALLENGED = 'user_challenged',
           USER_ENROLLED = 'user_enrolled',
+          USER_DISENROLLED = 'user_disenrolled',
           METHOD_REQUIRES_SETUP = 'requires_setup',
           NO_ENROLMENT_IN_PROGRESS = 'no_enrolment_in_progress';
+
+     public function begin(Challengeable $user, $method_name, array $meta = []);
+     public function beforeSetup(Challengeable $user, $method_name);
+     public function setup(Challengeable $user, $method_name, $token = null, array $meta = []);
+     public function enrol(Challengeable $user, $method);
+     public function disenrol(Challengeable $user, $method);
+     public function challenge(Challengeable $user, $method_name, $purpose, array $meta = []);
+     public function verify(Challengeable $user, $method, $token);
+     public function canChallenge(Challengeable $user, $method, $purpose);
+     public function canEnrol(Challengeable $user, $method);
+     public function getEnrolledAuthDriverList(Challengeable $user);
+     public function setEventDispatcher(EventDispatcher $events);
+     public function getEventDispatcher();
 }
