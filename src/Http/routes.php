3091b068e8ff13bc7951c6ac6bd13eb5aac58a49
Oliver Green <green2go@gmail.com>
2019-07-27 11:34:09 +0100

    Throttle routes.

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -2,12 +2,12 @@
 
 Route::get('/tfa', 'Auth\TwoFactorAuthController@showMethodSelectionForm')->name('tfa');
 Route::get('/tfa/error', 'Auth\TwoFactorAuthController@showError')->name('tfa.error');
-Route::post('/tfa/challenge', 'Auth\TwoFactorAuthController@challenge')->name('tfa.challenge');
+Route::post('/tfa/challenge', 'Auth\TwoFactorAuthController@challenge')->name('tfa.challenge')->middleware('throttle:5,1');
 Route::get('/tfa/{method}/verify', 'Auth\TwoFactorAuthController@showVerificationForm')->name('tfa.verify.form');
-Route::post('/tfa/{method}/verify', 'Auth\TwoFactorAuthController@verify');
-Route::get('/tfa/{method}/enrol', 'Auth\TwoFactorAuthController@begin')->name('tfa.enrol');
-Route::get('/tfa/{method}/enrol/setup', 'Auth\TwoFactorAuthController@showSetupForm')->name('tfa.enrolment.setup');
-Route::post('/tfa/{method}/enrol/setup', 'Auth\TwoFactorAuthController@setup');
+Route::post('/tfa/{method}/verify', 'Auth\TwoFactorAuthController@verify')->middleware('throttle:10,1');
+Route::get('/tfa/{method}/enrol', 'Auth\TwoFactorAuthController@begin')->name('tfa.enrol')->middleware('throttle:10,1');
+Route::get('/tfa/{method}/enrol/setup', 'Auth\TwoFactorAuthController@showSetupForm')->name('tfa.enrolment.setup')->middleware('throttle:10,1');
+Route::post('/tfa/{method}/enrol/setup', 'Auth\TwoFactorAuthController@setup')->middleware('throttle:10,1');
 Route::get('/tfa/{method}/enrolled', 'Auth\TwoFactorAuthController@showEnrolled')->name('tfa.enrolled');
 Route::get('/tfa/{method}/disenrol', 'Auth\TwoFactorAuthController@disenrol');
 Route::get('/tfa/{method}/disenrolled', 'Auth\TwoFactorAuthController@showDisenrolled')->name('tfa.disenrolled');
