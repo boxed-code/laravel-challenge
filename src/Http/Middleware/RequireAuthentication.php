575bbc68a4a60db5082a9c8217d7b0a14bba5a3c
Oliver Green <green2go@gmail.com>
2020-04-19 09:29:55 +0000

    Apply fixes from StyleCI

--- src/Http/Middleware/RequireAuthentication.php
+++ src/Http/Middleware/RequireAuthentication.php
@@ -95,7 +95,7 @@ class RequireAuthentication
     protected function shouldAuthenticate($request, $purposes, $lifetime)
     {
         return
-            ! is_null($request->user()) &&
+            !is_null($request->user()) &&
             !$this->inExceptArray($request) &&
             $this->manager->shouldEnforceFor($request->user()) &&
             !$this->manager->isAuthenticated($request->user(), null, $purposes, $lifetime);
